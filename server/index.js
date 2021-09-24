11572b2a398da986e79a409e549fe537876296e6
Tuan Tai <nguyentuantai412000@gmail.com>
2021-09-24 16:01:43 +0700

    crud user authentication & authorization

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
 
 //Import Route
-const authRouter = require("./routes/auth");
+const authRouter = require("./routes/auth")
+const userRouter = require("./routes/user")
 
-require("dotenv").config();
-const PORT = 5000;
+require("dotenv").config()
+const PORT = 5000
 
 //Connect to DB
 const connectDB = async () => {
@@ -14,19 +15,20 @@ const connectDB = async () => {
     await mongoose.connect(process.env.DB_CONNECT, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
-    });
-    console.log("MongoDb connected");
+    })
+    console.log("MongoDb connected")
   } catch (error) {
-    console.log(error);
-    process.exit(1);
+    console.log(error)
+    process.exit(1)
   }
-};
-connectDB();
+}
+connectDB()
 
 //Middleware
-app.use(express.json());
+app.use(express.json())
 
 //Routes Middleware
-app.use("/api/auth", authRouter);
+app.use("/api/auth", authRouter)
+app.use("/api/user", userRouter)
 
-app.listen(PORT, () => console.log(`Server started on post ${5000}`));
+app.listen(PORT, () => console.log(`Server started on post ${5000}`))

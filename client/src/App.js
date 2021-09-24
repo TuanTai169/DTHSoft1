ca8f65a8e85670a4f7ea1196abbee0753029abac
Tuan Tai <nguyentuantai412000@gmail.com>
2021-09-24 16:29:13 +0700

    clean source

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import "./App.css"
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import HomePage from "./components/HomePage/HomePage"
+import LoginPage from "./components/Login/LoginPage"
 function App() {
   return (
-    <div className="App">
-      <h1>DTH Shop</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" component={HomePage} />
+        <Route path="/login" component={LoginPage} />
+      </Switch>
+    </Router>
   )
 }
 

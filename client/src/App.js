25989b53eb75aaef931790858efdd3e7060443ce
Tuan Tai <nguyentuantai412000@gmail.com>
2021-09-26 18:54:49 +0700

    restore fe

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,16 @@
 import "./App.css"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import HomePage from "./components/HomePage/HomePage"
-import AuthContextProvider from "./contexts/AuthContext"
 import LoginPage from "./components/Login/LoginPage"
 
 function App() {
   return (
-    <AuthContextProvider>
-      <Router>
-        <Switch>
-          <Route path="/login" component={LoginPage} />
-          <Route path="/dashboard" component={HomePage} />
-        </Switch>
-      </Router>
-    </AuthContextProvider>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/login" component={LoginPage} />
+      </Switch>
+    </Router>
   )
 }
 
--- client/src/components/Login/LoginPage.js
+++ client/src/components/Login/LoginPage.js
@@ -1,69 +1,8 @@
 import "./loginPage.css"
 import React from "react"
-import { Link, useHistory, Redirect } from "react-router-dom"
-import { useState, useContext } from "react"
-import { AuthContext } from "../../contexts/AuthContext"
 
 function LoginPage() {
-  //Context
-  const { loginUser } = useContext(AuthContext)
-
-  //Route
-  const history = useHistory()
-
-  //Local State
-  const [login, setLogin] = useState({
-    email: "",
-    password: "",
-  })
-
-  const { email, password } = login
-
-  const onChangeLogin = (event) =>
-    setLogin({ ...login, [event.target.name]: event.target.value })
-
-  const onLogin = async (event) => {
-    event.preventDefault()
-
-    try {
-      const loginData = await loginUser(login)
-      if (loginData.success) {
-        history.push("/dashboard")
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  return (
-    <>
-      <form className="login-form" autoComplete="off" onSubmit={onLogin}>
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          required
-          value={email}
-          onChange={onChangeLogin}
-        />
-        <div className="input-icon">
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            required
-            value={password}
-            onChange={onChangeLogin}
-          />
-          <i className="fa fa-eye show-password"></i>
-        </div>
-        <Link to="#" className="forgot">
-          Forgot password?
-        </Link>
-        <button type="submit">Sign in</button>
-      </form>
-    </>
-  )
+  return <h1>Login Page</h1>
 }
 
 export default LoginPage

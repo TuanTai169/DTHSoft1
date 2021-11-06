7619164b53b75fbbc5d4f0b7e50608c94a96d2b8
TuanTai169 <nguyentuantai412000@gmail.com>
2021-11-06 20:45:43 +0700

    list booking

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -2,12 +2,14 @@ import themeReducer from "./themeReducer"
 import serviceReducer from "./serviceReducer"
 import roomReducer from "./roomReducer"
 import authReducer from "./authReducer"
+import bookingReducer from "./bookingReducer"
 import { combineReducers } from "redux"
 
 const rootReducer = combineReducers({
   themeReducer,
   serviceReducer,
   roomReducer,
+  bookingReducer,
   auth: authReducer,
 })
 

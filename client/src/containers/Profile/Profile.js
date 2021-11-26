2179e9816f5c85d7e0de591bac3f7ae5545c54ac
dream2216 <76442774+dream2216@users.noreply.github.com>
2021-11-26 16:27:50 +0700

    receipt

--- client/src/containers/Profile/Profile.js
+++ client/src/containers/Profile/Profile.js
@@ -1,43 +1,43 @@
-import "./profile.css"
-import React, { Fragment, useEffect } from "react"
-import { Button, Row, Col } from "react-bootstrap"
-import { useDispatch, useSelector } from "react-redux"
-import { useState } from "react"
-import EditProfileModal from "./EditProfileModal"
-import { loadUser } from "./../../redux/actions/authAction"
-import axios from "axios"
-import { HOST_API_URL } from "../../redux/constants/api"
-import { toast } from "react-toastify"
-import FullLoading from "../../components/Common/FullLoading/FullLoading"
+import "./Profile.css";
+import React, { Fragment, useEffect } from "react";
+import { Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import EditProfileModal from "./EditProfileModal";
+import { loadUser } from "./../../redux/actions/authAction";
+import axios from "axios";
+import { HOST_API_URL } from "../../redux/constants/api";
+import { toast } from "react-toastify";
+import FullLoading from "../../components/Common/FullLoading/FullLoading";
 
 function Profile() {
-  const user = useSelector((state) => state.auth.user)
-  const users = useSelector((state) => state.userReducer.users)
-  const isLoading = useSelector((state) => state.userReducer.isUserLoading)
-  const role = useSelector((state) => state.auth.user.roles)
-  const dispatch = useDispatch()
+  const user = useSelector((state) => state.auth.user);
+  const users = useSelector((state) => state.userReducer.users);
+  const isLoading = useSelector((state) => state.userReducer.isUserLoading);
+  const role = useSelector((state) => state.auth.user.roles);
+  const dispatch = useDispatch();
 
-  useEffect(() => dispatch(loadUser()), [dispatch, users, user])
+  useEffect(() => dispatch(loadUser()), [dispatch, users, user]);
 
-  const [isEditOpen, setIsEditOpen] = useState(false)
-  const [avatar, setAvatar] = useState(false)
+  const [isEditOpen, setIsEditOpen] = useState(false);
+  const [avatar, setAvatar] = useState(false);
 
-  const handlerModalEditClose = () => setIsEditOpen(false)
+  const handlerModalEditClose = () => setIsEditOpen(false);
 
   const changeAvatar = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      const file = e.target.files[0]
+      const file = e.target.files[0];
 
-      if (!file) toast.error("No files were uploaded")
+      if (!file) toast.error("No files were uploaded");
 
-      if (file.size > 1024 * 1024) toast.error("Size too large")
+      if (file.size > 1024 * 1024) toast.error("Size too large");
 
       if (file.type !== "image/jpeg" && file.type !== "image/png")
-        toast.error("File format is incorrect")
+        toast.error("File format is incorrect");
 
-      let formData = new FormData()
-      formData.append("file", file)
+      let formData = new FormData();
+      formData.append("file", file);
 
       const res = await axios.post(
         `${HOST_API_URL}/user/upload-avatar/${user._id}`,
@@ -47,13 +47,13 @@ function Profile() {
             "Content-Type": "multipart/form-data",
           },
         }
-      )
-      toast.success(res.data.message)
-      setAvatar(res.data.url)
+      );
+      toast.success(res.data.message);
+      setAvatar(res.data.url);
     } catch (err) {
-      err.response && toast.error(err.response.data.message)
+      err.response && toast.error(err.response.data.message);
     }
-  }
+  };
 
   return (
     <>
@@ -163,7 +163,7 @@ function Profile() {
         </Fragment>
       )}
     </>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
